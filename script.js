6a1a948517ed9adc19bcbbd16566c2125fb19b7d
Laura Hofer <ljhofer@gmail.com>
2021-09-28 17:37:35 -0500

    Added event listener function and  deleted extraneous div tags

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
 // Variables for lefthand time slots
-var nineAM = $("#9Am");
-var tenAM = $("#10Am");
-var elevenAM = $("#11Am");
-var twelvePM = $("#12Pm");
-var onePM = $("#1Pm");
-var twoPM = $("#2Pm");
-var threePM = $("#3Pm");
-var fourPM = $("#4Pm");
+// var nineAM = $("#9Am");
+// var tenAM = $("#10Am");
+// var elevenAM = $("#11Am");
+// var twelvePM = $("#12Pm");
+// var onePM = $("#1Pm");
+// var twoPM = $("#2Pm");
+// var threePM = $("#3Pm");
+// var fourPM = $("#4Pm");
 
 //Variables for text fields
-var nineInput = $("#input9AM");
+var nineInput = $("#input-9AM");
 var tenInput = $("#input10AM");
 var elevenInput = $("#input11AM");
 var twelveInput = $("#input12PM");
@@ -35,32 +35,35 @@ var calendarEvents = {
     fourInput: fourInput.val(),
 }
 
+// TODO:Add function for populated calendarEvents from local storage
+
 
 // Event listener for click on image class
-saveBtn.on("click", function() {
-    console.log("Click function running");
-    saveEventData();
+saveBtn.on("click", function(event) {
+    event.preventDefault();
+    var siblingInput = ($(this).siblings("input")[0]); //Grabbing the sibling input of whatever button is pressed
+    console.log(this);
+    console.log(siblingInput);
+    saveEventData(siblingInput);
     
-
 })
 
 
 // Save imput fields as JSON string
-function saveEventData() { 
-    console.log("Save function running");
-    renderEventData();
+function saveEventData(siblingInput) { 
+    var eventTime = siblingInput.id.split("-")[1];
 
-
+    // TODO: Based on eventTime update a property of calendarEvents
+    localStorage.setItem("calendarEvents", JSON.stringify(calendarEvents));
+    // renderEventData();
 }
 
 
 
 // Display JSON string back in planner
-function renderEventData () {
-    console.log("Render function running");
-    console.log(nineInput.val());
-
-}
+// function renderEventData () {
+//     console.log("Render function running");
+// }
 
 //Keep the date current
 
