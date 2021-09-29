9a4f85b35eff3127b6b8a87e1db7a37fc97cd659
Laura Hofer <ljhofer@gmail.com>
2021-09-28 20:37:24 -0500

    Added if then statements for local storage and styled with Bootstrap

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
 
-// Variables for lefthand time slots
-// var nineAM = $("#9Am");
-// var tenAM = $("#10Am");
-// var elevenAM = $("#11Am");
-// var twelvePM = $("#12Pm");
-// var onePM = $("#1Pm");
-// var twoPM = $("#2Pm");
-// var threePM = $("#3Pm");
-// var fourPM = $("#4Pm");
 
 //Variables for text fields
 var nineInput = $("#input-9AM");
-var tenInput = $("#input10AM");
-var elevenInput = $("#input11AM");
-var twelveInput = $("#input12PM");
-var oneInput = $("#input1PM");
-var twoInput = $("#input2PM");
-var threeInput = $("#input3PM");
-var fourInput = $("#input4PM");
+var tenInput = $("#input-10AM");
+var elevenInput = $("#input-11AM");
+var twelveInput = $("#input-12PM");
+var oneInput = $("#input-1PM");
+var twoInput = $("#input-2PM");
+var threeInput = $("#input-3PM");
+var fourInput = $("#input-4PM");
 
 // Variable for right hand image class
 var saveBtn = $(".saveBtn");
 
 // Variable for local storage functions
-
 var calendarEvents = {
     nineInput: nineInput.val(), 
     tenInput: tenInput.val(), 
@@ -42,29 +32,40 @@ var calendarEvents = {
 saveBtn.on("click", function(event) {
     event.preventDefault();
     var siblingInput = ($(this).siblings("input")[0]); //Grabbing the sibling input of whatever button is pressed
-    console.log(this);
-    console.log(siblingInput);
     saveEventData(siblingInput);
-    
 })
 
 
 // Save imput fields as JSON string
 function saveEventData(siblingInput) { 
     var eventTime = siblingInput.id.split("-")[1];
+    
+    // TODO: 
+    //Figure out which has hour has new text eventTime
+    if (eventTime === "9AM") {
+        calendarEvents.nineInput = siblingInput.value;
+    } else if (eventTime === "10AM") {
+        calendarEvents.tenInput = siblingInput.value;
+    } else if (eventTime === "11AM") {
+        calendarEvents.elevenInput = siblingInput.value;
+    } else if (eventTime === "12PM") {
+        calendarEvents.twelveInput = siblingInput.value;
+    } else if (eventTime === "1PM") {
+        calendarEvents.oneInput = siblingInput.value;
+    } else if (eventTime === "2PM") {
+        calendarEvents.twoInput = siblingInput.value;
+    } else if (eventTime === "3PM") {
+        calendarEvents.threeInput = siblingInput.value;
+    } else
+         calendarEvents.fourInput = siblingInput.value;
+   
 
-    // TODO: Based on eventTime update a property of calendarEvents
+    //Update the global variable with that new text
+    //Push calendarEvents back to local storage
     localStorage.setItem("calendarEvents", JSON.stringify(calendarEvents));
-    // renderEventData();
+
 }
+    
 
+//Add moment functionality
 
-
-// Display JSON string back in planner
-// function renderEventData () {
-//     console.log("Render function running");
-// }
-
-//Keep the date current
-
-//Change the colors
